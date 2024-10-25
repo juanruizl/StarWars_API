6e0318a29f7295f8e99dda43975e6ef95324140a
juanruizl <160032371+juanruizl@users.noreply.github.com>
2024-10-25 14:34:08 +0000

    api_star_wars_1

--- src/app.py
+++ src/app.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Character, Planet, Starship, Favorite
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -26,26 +25,149 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/user', methods=['GET', 'POST'])
+def handle_user():
+    if request.method == 'GET':
+        users = [user.to_dit() for user in User.query.all()]
+        return jsonify(users), 200
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_user = User(**data)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(new_user.to_dit()), 201
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route("/user/<int:id>", methods=["GET", "PUT", "DELETE"])
+def modify_user(id):
+    user = User.query.get(id)
+    if not user:
+        return jsonify({"msg": "User not found"}), 404
+    if request.method == 'GET':
+        return jsonify(user.to_dit()), 200
+    elif request.method == 'PUT':
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(user, key, value)
+        db.session.commit()
+        return jsonify(user.to_dit()), 200
+    elif request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"msg": "User deleted"}), 204
 
-    return jsonify(response_body), 200
+@app.route('/character', methods=['GET', 'POST'])
+def handle_character():
+    if request.method == 'GET':
+        characters = [character.to_dit() for character in Character.query.all()]
+        return jsonify(characters), 200
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_character = Character(**data)
+        db.session.add(new_character)
+        db.session.commit()
+        return jsonify(new_character.to_dit()), 201
 
-# this only runs if `$ python src/app.py` is executed
+@app.route("/character/<int:id>", methods=["PUT", "DELETE"])
+def modify_character(id):
+    character = Character.query.get(id)
+    if not character:
+        return jsonify({"msg": "Character not found"}), 404
+    if request.method == 'PUT':
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(character, key, value)
+        db.session.commit()
+        return jsonify(character.to_dit()), 200
+    elif request.method == 'DELETE':
+        db.session.delete(character)
+        db.session.commit()
+        return jsonify({"msg": "Character deleted"}), 204
+
+@app.route('/planet', methods=['GET', 'POST'])
+def handle_planet():
+    if request.method == 'GET':
+        planets = [planet.to_dit() for planet in Planet.query.all()]
+        return jsonify(planets), 200
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_planet = Planet(**data)
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(new_planet.to_dit()), 201
+
+@app.route("/planet/<int:id>", methods=["PUT", "DELETE"])
+def modify_planet(id):
+    planet = Planet.query.get(id)
+    if not planet:
+        return jsonify({"msg": "Planet not found"}), 404
+    if request.method == 'PUT':
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(planet, key, value)
+        db.session.commit()
+        return jsonify(planet.to_dit()), 200
+    elif request.method == 'DELETE':
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify({"msg": "Planet deleted"}), 204
+
+@app.route('/starship', methods=['GET', 'POST'])
+def handle_starship():
+    if request.method == 'GET':
+        starships = [starship.to_dit() for starship in Starship.query.all()]
+        return jsonify(starships), 200
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_starship = Starship(**data)
+        db.session.add(new_starship)
+        db.session.commit()
+        return jsonify(new_starship.to_dit()), 201
+
+@app.route("/starship/<int:id>", methods=["PUT", "DELETE"])
+def modify_starship(id):
+    starship = Starship.query.get(id)
+    if not starship:
+        return jsonify({"msg": "Starship not found"}), 404
+    if request.method == 'PUT':
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(starship, key, value)
+        db.session.commit()
+        return jsonify(starship.to_dit()), 200
+    elif request.method == 'DELETE':
+        db.session.delete(starship)
+        db.session.commit()
+        return jsonify({"msg": "Starship deleted"}), 204
+
+@app.route('/user/<int:id>/favorites', methods=['GET'])
+def get_favorites(id):
+    favorites = Favorite.query.filter_by(user_id=id).all()
+    return jsonify([favorite.to_dit() for favorite in favorites]), 200
+
+@app.route('/favorite', methods=['POST'])
+def add_favorite():
+    data = request.get_json()
+    favorite = Favorite(**data)
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify(favorite.to_dit()), 201
+
+@app.route('/favorite/<int:id>', methods=['DELETE'])
+def delete_favorite(id):
+    favorite = Favorite.query.get(id)
+    if not favorite:
+        return jsonify({"msg": "Favorite not found"}), 404
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify({"msg": "Favorite deleted"}), 204
+
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,139 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    subscription_date = Column(DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    favorites = relationship('Favorite', backref='user', lazy=True)
 
-    def serialize(self):
+    def to_dit(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "subscription_date": self.subscription_date
+        }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=True)
+    mass = Column(String(250), nullable=True)
+    hair_color = Column(String(250), nullable=True)
+    skin_color = Column(String(250), nullable=True)
+    eye_color = Column(String(250), nullable=True)
+    birth_year = Column(String(250), nullable=True)
+    gender = Column(String(250), nullable=True)
+    homeworld = Column(String(250), nullable=True)
+
+    favorites = relationship('Favorite', backref='character', lazy=True)
+
+    def to_dit(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "homeworld": self.homeworld
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=True)
+    rotation_period = Column(String(250), nullable=True)
+    orbital_period = Column(String(250), nullable=True)
+    gravity = Column(String(250), nullable=True)
+    population = Column(String(250), nullable=True)
+    climate = Column(String(250), nullable=True)
+    terrain = Column(String(250), nullable=True)
+
+    favorites = relationship('Favorite', backref='planet', lazy=True)
+
+    def to_dit(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain
+        }
+
+class Starship(db.Model):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=True)
+    starship_class = Column(String(250), nullable=True)
+    manufacturer = Column(String(250), nullable=True)
+    cost_in_credits = Column(String(250), nullable=True)
+    length = Column(String(250), nullable=True)
+    crew = Column(String(250), nullable=True)
+    passengers = Column(String(250), nullable=True)
+    max_atmosphering_speed = Column(String(250), nullable=True)
+    hyperdrive_rating = Column(String(250), nullable=True)
+    MGLT = Column(String(250), nullable=True)
+    cargo_capacity = Column(String(250), nullable=True)
+    consumables = Column(String(250), nullable=True)
+
+    favorites = relationship('Favorite', backref='starship', lazy=True)
+
+    def to_dit(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "starship_class": self.starship_class,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "hyperdrive_rating": self.hyperdrive_rating,
+            "MGLT": self.MGLT,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    starship_id = Column(Integer, ForeignKey('starship.id'), nullable=True)
+
+    def to_dit(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
+            "starship_id": self.starship_id
         }
